9c4e71486f8885cd0b8b732388e95fadb1627c42
samirvithlani <samir.vithlani83955@gmail.com>
2023-09-18 09:45:04 +0530

    basics

---
+++ basics/basics1.py
@@ -0,0 +1,23 @@
+#printf # is used for single line comment
+print("hello python",end =" ")
+print("hello all !!!")
+
+#what is variable ? varibale is a container which stores the value
+# a is a variable
+a = 10
+print(a)
+print("value of a  = ",a)
+print("type of a = ",type(a))
+
+per = 78.89
+print("per = ",per)
+print("type of per = ",type(per))
+
+
+name = "python"
+print("value of name = ",name)
+print("type of name = ",type(name))
+
+isActive = False
+print("value of isActive = ",isActive)
+print("type of isActive = ",type(isActive))
